069128ad4e0f6a07e803df3e6046b1ca0c36541d
MattParki <mattparkinson_12@hotmail.com>
2021-04-12 15:54:58 +0100

    first files added, adding jquery and a button to the page

---
+++ curlrequest.php
@@ -0,0 +1,56 @@
+
+<script type="text/javascript">
+var wpwlOptions = {style:"card"}
+
+
+</script>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://test.oppwa.com/v1/paymentWidgets.js?checkoutId={checkoutId}"></script>
+    <title>Document</title>
+    
+
+<script>
+    $(document).ready(function() {
+        $("input").keyup(function() {
+            var name = $("input").val();
+            $.post()
+        })
+    })
+
+</script>
+
+
+</head>
+<body>
+
+
+
+</script>
+
+
+<button type="button">Click Me!</button>
+
+<div id="#div1" class= "container">
+
+
+</div>
+
+<form action="{shopperResultUrl}" class="paymentWidgets" data-brands="VISA MASTER AMEX"></form>
+
+
+
+<!-- JQuery and stripe CDN -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+
+</body>
+</html>
